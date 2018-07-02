ed770cdea60abc3a9e1d82bd80794e6145d04bb7
Pumin Swangjang <realxiz@pumins-mbp.lan>
2018-07-02 18:39:51 +0700

    fix cards

--- src/pages/Setting/Setting.js
+++ src/pages/Setting/Setting.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react'
 
-import { Container, TabContent, TabPane, Nav, NavItem, NavLink, Card, CardBody, Button, CardTitle, CardText, Row, Col } from 'reactstrap'
+import {
+	DropdownToggle,
+	Edit,
+	DropdownMenu,
+	DropdownItem,
+	ButtonDropdown,
+	Container,
+	TabContent,
+	TabPane,
+	Nav,
+	NavItem,
+	NavLink,
+	Card,
+	CardBody,
+	Button,
+	CardTitle,
+	CardText,
+	Row,
+	Col
+} from 'reactstrap'
 import classnames from 'classnames'
 import './Setting.css'
 import axios from 'axios'
 import { PersonAdd } from 'styled-icons/material/PersonAdd'
+import { MoreHoriz } from 'styled-icons/material/MoreHoriz'
+import { Link } from 'react-router-dom'
 
-// const PersonAdd = PersonAdd.extend`
-// 	width: 1.5rem;
-// 	height: 1.5rem;
-// 	color: white;
-// 	display: inline-block;
-// 	border-radius: 25%;
-// `
+const IconAdd = PersonAdd.extend`
+	width: 110px;
+	height: 110px;
+	color: gray;
+	display: inline-block;
+	border-radius: 10px;
+`
+
+const Editt = MoreHoriz.extend`
+	color: #5bc2e1;
+	width: 1.5625rem;
+	height: 1.5625rem;
+`
 
 class Setting extends Component {
 	constructor(props) {
@@ -21,6 +48,12 @@ class Setting extends Component {
 			activeTab: '0',
 			users: []
 		}
+	}
+
+	toggle() {
+		this.setState({
+			dropdownOpen: !this.state.dropdownOpen
+		})
 	}
 
 	toggle = tab => {
@@ -56,10 +89,10 @@ class Setting extends Component {
 
 	render() {
 		return (
-			<Container className="mt-4">
+			<Container className="mt-5">
 				<Row>
 					<Col md={12}>
-						<Nav tabs>
+						<Nav tabs className="mx-5">
 							<NavItem>
 								<NavLink
 									className={classnames({ active: this.state.activeTab === '1' })}
@@ -94,21 +127,17 @@ class Setting extends Component {
 							</NavItem>
 						</Nav>
 
-						<TabContent activeTab={this.state.activeTab}>
+						<TabContent activeTab={this.state.activeTab} className="mx-5">
 							<TabPane tabId="1">
 								<Row>
-									{/* <Col sm={4} className="mb-2">
-								<Card body>
-									<CardTitle>Add</CardTitle>
-									<CardText>All</CardText>
-								</Card>
-							</Col> */}
 									<Col sm={4} className="h-150">
 										{/* mb-2 */}
 										<Card body>
 											<CardText>
 												<center>
-													<PersonAdd height="110px" />
+													<div>
+													<IconAdd />
+													</div>
 												</center>
 											</CardText>
 										</Card>
@@ -119,7 +148,42 @@ class Setting extends Component {
 											<Col sm={4} className="mb-4" key={users.id}>
 												<Card body className="h-150">
 													<CardTitle>
-														<div>{users.name}</div>
+														<div>
+															{users.name}
+															<ButtonDropdown
+																className="btn-secondary"
+																isOpen={this.state.btnDropright}
+																toggle={() => {
+																	this.setState({ btnDropright: !this.state.btnDropright })
+																}}
+															>
+																<DropdownToggle>
+																	<Editt />
+																</DropdownToggle>
+																<DropdownMenu className="dropdown-menu">
+																	<DropdownItem
+																		className="dropdownitem"
+																		style={{
+																			borderBottom: '1px solid #5bc2e1',
+																			borderRadius: '0.2rem 0.2rem 0 0'
+																		}}
+																	>
+																		<div>Edit Member</div>
+																	</DropdownItem>
+																	<Link to="/project" style={{ textDecoration: 'none' }}>
+																		<DropdownItem
+																			className="dropdowndeleteitem"
+																			style={{
+																				color: '#f67879',
+																				borderRadius: '0 0 0.2rem 0.2rem'
+																			}}
+																		>
+																			Delete
+																		</DropdownItem>
+																	</Link>
+																</DropdownMenu>
+															</ButtonDropdown>
+														</div>
 													</CardTitle>
 													<CardText>
 														<div style={{ display: 'flex', flexDirection: 'row' }}>
@@ -141,7 +205,7 @@ class Setting extends Component {
 										<Card body>
 											<CardText>
 												<center>
-													<PersonAdd height="110px" />
+													<IconAdd />
 												</center>
 											</CardText>
 										</Card>
@@ -173,7 +237,7 @@ class Setting extends Component {
 										<Card body>
 											<CardText>
 												<center>
-													<PersonAdd height="110px" />
+													<IconAdd/>
 												</center>
 											</CardText>
 										</Card>
