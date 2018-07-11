c3a2d12bc9166e3bedd0f6687e6b23ec59c755ab
ppangchn <pangpy_pang@hotmail.com>
2018-07-11 14:42:08 +0700

    add compare button

--- src/components/Views/Overview/UserDetail.js
+++ src/components/Views/Overview/UserDetail.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import { Button , Progress} from 'reactstrap'
+import { Button, Progress } from 'reactstrap'
 import AddProject from '../../../pages/AddProject/AddProject'
 import { Link } from 'react-router-dom'
-import {Line} from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2'
 import './UserDetail.css'
 import '../../../pages/ViewByProject/ProjectSidebar.css'
 
@@ -18,36 +18,11 @@ const Item = styled.div`
   background-color: #ffffff;
   border-bottom: 0.5px solid #fff6f5;
 `
-const data = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-  datasets: [
-    {
-      label: 'My First dataset',
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [65, 59, 80, 81, 56, 55, 40]
-    }
-  ]
-};
+
 class UserDetail extends Component {
   constructor(props) {
     super(props)
-    this.state = { toggleAddModal: false }
+    this.state = { toggleAddModal: false ,data:{}}
     this.update = this.update.bind(this)
   }
   update() {
@@ -56,12 +31,42 @@ class UserDetail extends Component {
   toggleAddModal(state) {
     this.setState({ toggleAddModal: state })
   }
+  componentDidMount() {
+    const data = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'Burn',
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: '#20aadb',
+          borderColor: '#98e3ff',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: '#20aadb',
+          pointBackgroundColor: '#20aadb',
+          pointBorderWidth: 5,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: '#20aadb',
+          pointHoverBorderColor: '#20aadb',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: this.props.graph
+        }
+      ]
+    }
+    this.setState({data})
+  }
   render() {
     const { projectTimeline } = this.props
     return (
       <Card className="userdetail">
         {this.props.name}
-        <Line data={data} />
+        <Line data={this.state.data} />
+        {/* <Scatter data={data}/> */}
         {projectTimeline.map(project => {
           return (
             <Item className="overviewprojectitem">
@@ -80,7 +85,7 @@ class UserDetail extends Component {
                 >
                   {project.project.name}
                 </Link>
-                <div style={{float: 'right'}}>{project.project.process}%</div>
+                <div style={{ float: 'right' }}>{project.project.process}%</div>
                 <Progress
                   className="overviewprogress"
                   color={String(project.project.color).substring(1)}
