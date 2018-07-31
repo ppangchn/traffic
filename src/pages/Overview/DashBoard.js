132610b8d8540c70fe8f6bacc0a0b99141d6e10a
Pumin Swangjang <realxiz@pumins-mbp.lan>
2018-07-31 11:29:52 +0700

    add getToken

--- src/pages/Overview/DashBoard.js
+++ src/pages/Overview/DashBoard.js
@@ -7,67 +7,74 @@ import { Button } from 'reactstrap'
 import { Timeline as TimelineIcon } from 'styled-icons/material/Timeline'
 import { Link } from 'react-router-dom'
 import './DashBoard.css'
-
+import auth from '../../service/index'
 
 const Timeline = TimelineIcon.extend`
-  color: #5bc2e1;
+	color: #5bc2e1;
 `
 const Container = styled.div`
-  background-color: #f1f5f8;
-  -webkit-background-size: cover;
-  background-size: cover;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
+	background-color: #f1f5f8;
+	-webkit-background-size: cover;
+	background-size: cover;
+	display: flex;
+	flex-direction: column;
+	height: 100vh;
 `
 const UserContainer = styled.div`
-  overflow-y: hidden;
-  overflow-x: scroll;
-  display: flex;
+	overflow-y: hidden;
+	overflow-x: scroll;
+	display: flex;
 `
 class DashBoard extends Component {
-  constructor(props) {
-    super(props)
+	constructor(props) {
+		super(props)
     this.state = { data: [] }
+    
   }
 
-  componentDidMount() {
-    axios.get(`${url}/users/pm`).then(res => {
-      const { data } = res
-      this.setState({ data })
-    })
+
+	componentDidMount() {
+		axios.get(`${url}/users/pm`).then(res => {
+			const { data } = res
+      this.setState({ data }) 
+      const user = auth.getToken()
+      const userDecoded = auth.decodeToken(user)
+      console.log('USERRRRRRRR',userDecoded);
+    }) 
   }
-  render() {
-    return (
-      <Container>
-        <UserContainer>
-          {this.state.data.map(user => {
-            let graph = []
-            user.projectManagement.map(timeline => {
-              if (!timeline.project.isDisable) graph.push(timeline.weight)
-            })
-            return (
-              <div key={user.id}>
-                <UserDetail
-                  id={user.id}
-                  name={user.name}
-                  roles={user.roles}
-                  graph={graph}
-                  projectManagement={user.projectManagement}
-                  updateHeader={this.props.updateHeader}
-                />
-              </div>
-            )
-          })}
-        </UserContainer>
-        <Link to="/overview/compare">
-          <Button color="compare">
-            <Timeline className="timelineicon" /> Compare
-          </Button>
-        </Link>
-      </Container>
-    )
-  }
+  
+  
+	render() {
+		return (
+			<Container>
+				<UserContainer>
+					{this.state.data.map(user => {
+						let graph = []
+						user.projectManagement.map(timeline => {
+							if (!timeline.project.isDisable) graph.push(timeline.weight)
+						})
+						return (
+							<div key={user.id}>
+								<UserDetail
+									id={user.id}
+									name={user.name}
+									roles={user.roles}
+									graph={graph}
+									projectManagement={user.projectManagement}
+									updateHeader={this.props.updateHeader}
+								/>
+							</div>
+						)
+					})}
+				</UserContainer>
+				<Link to="/overview/compare">
+					<Button color="compare">
+						<Timeline className="timelineicon" /> Compare
+					</Button>
+				</Link>
+			</Container>
+		)
+	}
 }
 
 export default DashBoard
