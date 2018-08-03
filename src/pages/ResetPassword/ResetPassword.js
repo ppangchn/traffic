312f4c059920f6c100f3e88ef9fa6f591e6c4318
Pumin Swangjang <realxiz@pumins-mbp.lan>
2018-08-03 13:06:32 +0700

    detect resetPass CheckToken

--- src/pages/ResetPassword/ResetPassword.js
+++ src/pages/ResetPassword/ResetPassword.js
@@ -1,14 +1,5 @@
 import React, { Component } from 'react'
-import {
-  Form,
-  Button,
-  Modal,
-  ModalHeader,
-  ModalBody,
-  Input,
-  FormFeedback,
-  ModalFooter
-} from 'reactstrap'
+import { Form, Button, Modal, ModalHeader, ModalBody, Input, FormFeedback, ModalFooter } from 'reactstrap'
 import { Link } from 'react-router-dom'
 import './ResetPassword.css'
 import { Key } from 'styled-icons/fa-solid/Key'
@@ -17,9 +8,9 @@ import url from '../../url'
 import axios from 'axios'
 
 const KeyReset = Key.extend`
-  width: 50px;
-  height: 50px;
-  color: #5bc2e1;
+	width: 50px;
+	height: 50px;
+	color: #5bc2e1;
 `
 const Container = styled.div`
 width: 100vw
@@ -30,88 +21,95 @@ display: flex;
 	background-color: white;
 `
 export default class ResetPassword extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      token: '',
-      fpass: '',
-      spass: '',
-      massage: ''
-    }
-  }
+	constructor(props) {
+		super(props)
+		this.state = {
+			token: '',
+			fpass: '',
+			spass: '',
+			massage: ''
+		}
+	}
 
-  submit = e => {
-    e.preventDefault()
-    try {
-      const data = {
-        resetToken: this.state.token,
-        password: this.state.spass
-      }
-      if (this.state.fpass.length > 5 && this.state.spass.length > 5) {
-        if (this.state.fpass == this.state.spass) {
-          axios.post(`${url}/users/resetpass`, data)
-          this.props.history.push(`/`)
-        } else {
-          this.setState({ massage: 'Please fill in the same password' })
-        }
-      } else {
-        this.setState({
-          massage: 'Password should contain at least 6 characters'
-        })
-      }
-    } catch (error) {}
-  }
+	submit = e => {
+		e.preventDefault()
+		try {
+			const data = {
+				resetToken: this.state.token,
+				password: this.state.spass
+			}
+			if (this.state.fpass.length > 5 && this.state.spass.length > 5) {
+				if (this.state.fpass == this.state.spass) {
+					axios.post(`${url}/users/resetpass`, data).then($res => {
+            console.log('RESSSSSSS',$res);
+            
+						const { data } = $res
+						this.setState({ message: data.message })
+						if (data.message != 'Token not found or expired!') {
+							// this.props.history.push(`/`)
+            }
+            else {
+              this.setState({ massage: 'Token not found or expired!' })
+            }
+					})
+				} else {
+					this.setState({ massage: 'Please fill in the same password' })
+				}
+			} else {
+				this.setState({
+					massage: 'Password should contain at least 6 characters'
+				})
+			}
+		} catch (error) {}
+	}
 
-  handleInputChange = e => {
-    const { name, value } = e.target
-    this.setState({ [name]: value })
-    this.setState({ massage: '' })
-    console.log({ [name]: value })
-  }
+	handleInputChange = e => {
+		const { name, value } = e.target
+		this.setState({ [name]: value })
+		this.setState({ massage: '' })
+		console.log({ [name]: value })
+	}
 
-  componentDidMount = () => {
-    if (this.props.match.params.token) {
-      this.setState({ token: this.props.match.params.token })
-    }
-  }
-  render() {
-    return (
-      <Container>
-        <div className="resetblock">
-          <KeyReset />
-          <br />
-          <div style={{ fontSize: '30px', lineHeight: '0', color: '#4a4a4a' ,marginTop:'15px'}}>
-            Reset Password?
-          </div>{' '}
-          <br />
-          <div class="please">Please enter your new password</div>
-          {/* <Input placeholder="example@pirsquare.net"/> */}
-          <Form onSubmit={this.submit}>
-            <input
-              style={{ fontSize: '8px !important' }}
-              name="fpass"
-              type="password"
-              className="inputform"
-              placeholder="New Password"
-              onChange={this.handleInputChange}
-              value={this.state.fpass}
-            />
-            <input
-              style={{ fontSize: '8px !important' }}
-              name="spass"
-              type="password"
-              className="inputform"
-              placeholder="Confirm Password"
-              onChange={this.handleInputChange}
-              value={this.state.spass}
-            />
-            <Button color="submit" size="lg" block>
-              Submit
-            </Button>
-          </Form>
-          <div className="err">{this.state.massage}</div>
-        </div>
-      </Container>
-    )
-  }
+	componentDidMount = () => {
+		if (this.props.match.params.token) {
+			this.setState({ token: this.props.match.params.token })
+		}
+	}
+	render() {
+		return (
+			<Container>
+				<div className="resetblock">
+					<KeyReset />
+					<br />
+					<div style={{ fontSize: '30px', lineHeight: '0', color: '#4a4a4a', marginTop: '15px' }}>Reset Password?</div> <br />
+					<div class="please">Please enter your new password</div>
+					{/* <Input placeholder="example@pirsquare.net"/> */}
+					<Form onSubmit={this.submit}>
+						<input
+							style={{ fontSize: '8px !important' }}
+							name="fpass"
+							type="password"
+							className="inputform"
+							placeholder="New Password"
+							onChange={this.handleInputChange}
+							value={this.state.fpass}
+						/>
+						<input
+							style={{ fontSize: '8px !important' }}
+							name="spass"
+							type="password"
+							className="inputform"
+							placeholder="Confirm Password"
+							onChange={this.handleInputChange}
+							value={this.state.spass}
+						/>
+						<Button color="submit" size="lg" block>
+							Submit
+						</Button>
+					</Form>
+					<div className="err">{this.state.massage}</div>
+				</div>
+			</Container>
+		)
+	}
 }
